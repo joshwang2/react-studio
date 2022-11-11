5c9c19dc537cc7d0bb5b6f7076ae4b1992c86421
Joshua Wang <joshuayinyi@gmail.com>
2022-11-10 20:59:55 -0500

    work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,21 +10,35 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+  const [price, setPrice] = useState(0);
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
 
+  function addToCart(item) {
+    setCartItems([...cartItems, item]);
+    setPrice(price + item.price);
+  }
+
   return (
     <div className="App">
+
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+          <BakeryItem item={item} addToCart={addToCart} price={price} />
+          // <p>Bakery Item {index}</p> 
       ))}
 
       <div>
         <h2>Cart</h2>
+          {cartItems.map((item, index) => (<p>{item.name}</p>))}
         {/* TODO: render a list of items in the cart */}
       </div>
+
+      <p>
+        <p>Total price: ${Math.round(100*price)/100}</p>
+      </p>
     </div>
   );
 }
---
+++ src/BakeryItem.js
@@ -0,0 +1,16 @@
+// import "./BakeryItem.css";
+
+function BakeryItem(props) {
+
+    return(
+        <div className="BakeryItem">
+            <h3>{props.item.name}</h3>
+            <p>{props.item.description}</p>
+            <p>{props.item.price}</p>
+            <img alt="" src={props.item.image} />
+            <button onClick={() => {props.addToCart(props.item)}}>Add to cart!</button>
+        </div>
+    )
+}
+
+export default BakeryItem;
